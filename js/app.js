1a6e7be3ca142d97de2ce8e8b34efeaad8c530ec
mogedy <mogedytech@gmail.com>
2020-12-11 08:46:00 +0100

    new updates

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+            // var carName     =   "Math solution: ";
+            // var firstVal    =   15;
+            // var secondVal   =   3;
+            // var thirdVal    =   3;
+            // var fourthVal   =   46;
+            // var xVal        =   10;
+            // var result      =   firstVal * xVal**100 - secondVal * xVal**12 + thirdVal * xVal - fourthVal;
+            // var result2     =   carName + result
+
+            // if(secondVal >= thirdVal)
+            //     var ans = "Yes, it's greater than.";
+            // else
+            //     var ans = "No, it's not less than.";
+            // alert(ans);
+
+            var     comment     =   'WAEC result for Akanimo. Chemistry: ';
+            var     chm         =   300;
+            // var     phy         =   61;
+            // var     bio         =   55;
+            // var     agr         =   45;
+            // var     crk         =   50;
+            // var     eco         =   42;
+            // var     geo         =   70;
+
+            if(chm >= 200)
+                var result  =   "Write PUTME";
+            else
+                var result  =   "Failed";
+
+            // alert(comment + result);
+            document.getElementById("script").innerHTML = "My First JavaScript";
+
+            function directionManual() {
+               document.getElementById("direction").innerHTML   =   "Use bolt instead of normal keke"; 
+            }
+            
